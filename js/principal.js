506ab4a63539324f814983e50daf8dc24058bcdc
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-05-28 13:01:46 -0300

    Operadores Lógicos

--- js/principal.js
+++ js/principal.js
@@ -10,5 +10,23 @@ var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
 var tdImc = paciente.querySelector(".info-imc");
-var imc = peso / (altura * altura);
-tdImc.textContent = imc;
+
+var pesoEhValido = true;
+var alturaEhvalido = true;
+
+if (peso <= 0 || peso >= 1000) {
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso Inválido!";
+}
+
+if (altura <= 0 || altura >= 3.00) {
+    console.log("Altura inválida!");
+    alturaEhvalido = false;
+    tdImc.textContent = "Altura Inválida!";
+}
+
+if (alturaEhvalido && pesoEhValido) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
