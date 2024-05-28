be86d00ecaf85ac8b4cf7209d8a6bddcae631983
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-05-28 14:03:53 -0300

    Escutando eventos

--- js/principal.js
+++ js/principal.js
@@ -37,3 +37,8 @@ for (var i = 0; i < pacientes.length; i++) {
         tdImc.textContent = imc.toFixed(2);
     }
 }
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function () {
+    console.log("Oi, cliquei no botão");
+});
