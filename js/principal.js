6c1f8be86e22e9a4779f9994ec8e4516ecffbc12
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-05-28 13:26:52 -0300

    Replicando a validação e o cálculo do IMC para todos os pacientes

--- js/principal.js
+++ js/principal.js
@@ -1,32 +1,37 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for (var i = 0; i < pacientes.length; i++) {
+    
+    var paciente = pacientes[i];
+    
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-var tdImc = paciente.querySelector(".info-imc");
+    var tdImc = paciente.querySelector(".info-imc");
 
-var pesoEhValido = true;
-var alturaEhvalido = true;
+    var pesoEhValido = true;
+    var alturaEhvalido = true;
 
-if (peso <= 0 || peso >= 1000) {
-    console.log("Peso inválido!");
-    pesoEhValido = false;
-    tdImc.textContent = "Peso Inválido!";
-}
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso Inválido!";
+    }
 
-if (altura <= 0 || altura >= 3.00) {
-    console.log("Altura inválida!");
-    alturaEhvalido = false;
-    tdImc.textContent = "Altura Inválida!";
-}
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida!");
+        alturaEhvalido = false;
+        tdImc.textContent = "Altura Inválida!";
+    }
 
-if (alturaEhvalido && pesoEhValido) {
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
+    if (alturaEhvalido && pesoEhValido) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
 }
