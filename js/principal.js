427b428ea14ca82d91e1bf73bcb5ae10595e312b
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-05-28 11:59:05 -0300

    Calculando e exibindo o IMC corretamente

--- js/principal.js
+++ js/principal.js
@@ -6,9 +6,9 @@ var paciente = document.querySelector("#primeiro-paciente");
 var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
 
-var tdAltura = querySelector(".info-altura");
+var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-var imc = peso / altura * altura;
-
-console.log(imc);
+var tdImc = paciente.querySelector(".info-imc");
+var imc = peso / (altura * altura);
+tdImc.textContent = imc;
