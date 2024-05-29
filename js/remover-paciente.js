4b4c4e152c622030001e46bea273ea551674d11e
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-05-29 12:34:33 -0300

    Delegando eventos

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,8 +1,8 @@
 var pacientes = document.querySelectorAll(".paciente");
 
-pacientes.forEach(function(paciente) {
-    paciente.addEventListener("dblclick", function() {
-        console.log("fui clicado com duplo clique");
-        this.remove();
-    });
+var tabela = document.querySelector("table");
+tabela.addEventListener("dblclick", function(event) {
+    var alvoEvento = event.target;
+    var paiDoAlvo = alvoEvento.parentNode;
+    paiDoAlvo.remove();
 });
