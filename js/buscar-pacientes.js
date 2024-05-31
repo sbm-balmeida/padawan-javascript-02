935c8a507b80b3310e6daa6f8b06eb7ebc684259
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-05-31 10:06:07 -0300

    Capturando erros de requisição

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -6,12 +6,23 @@ botaoAdicionar.addEventListener("click", function() {
     xhr.open("GET", "https://raw.githubusercontent.com/loresgarcia/Pacientes-API/master/pacientes.json");
 
     xhr.addEventListener("load", function() {
-        var resposta = xhr.responseText;
+        var erroAjax = document.querySelector("#erro-ajax");
+
+        if (xhr.status == 200) {
+            erroAjax.classList.add("invisivel");
+            
+            var resposta = xhr.responseText;
     
-        var pacientes = JSON.parse(resposta);
-        pacientes.forEach(function(paciente) {
-            adicionaPacienteNaTabela(paciente);
-        });
+            var pacientes = JSON.parse(resposta);
+            pacientes.forEach(function(paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });
+        } else {
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+            
+            erroAjax.classList.remove("invisivel");
+        }       
     });
     xhr.send();
 });
